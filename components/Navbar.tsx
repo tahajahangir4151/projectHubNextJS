72fd0c59fb34336b2df066dc214169fa2b0423bb
tahajahangir4151 <139686825+tahajahangir4151@users.noreply.github.com>
2025-01-02 17:03:57 +0500

    Work on authentication with github(using authjs)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
@@ -15,13 +17,37 @@ const Navbar = () => {
           />
         </Link>
         <div className="flex items-center gap-5 text-black">
-          <Link href={"/"}>
-            <span className="text-2 font-sans max-sm:hidden">Create</span>
-          </Link>
-          <Link href={"/"}>
-            <span className="text-2 font-sans max-sm:hidden">Logout</span>
-          </Link>
-          <Link href={"/"}>Username</Link>
+          {session && session?.user ? (
+            <>
+              <Link href="/project/create">
+                <span className="text-2 font-sans max-sm:hidden">Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit" className="text-6 font-sans">
+                  Logout
+                </button>
+              </form>
+
+              <Link href={"/"}>{session.user?.name}</Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit" className="text-6 font-sans">
+                Login
+              </button>
+            </form>
+          )}
         </div>
       </nav>
     </header>
